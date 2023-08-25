cfbf1b25e44492af8cf4a99447ba3cea925378e1
Junaid Khan <me.zunaidkhan@gmail.com>
2023-08-25 16:24:42 +0530

    Updated fro Mobile view

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,26 +1,26 @@
-import React ,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Todoitems from "./Todoitems"
 const Todo = () => {
-    const initial = ()=>{
-       const lists =  localStorage.getItem("Todos");
-       if(lists){
-        return JSON.parse(lists);
-       }else{
-        return [ ];
-       }
+    const initial = () => {
+        const lists = localStorage.getItem("Todos");
+        if (lists) {
+            return JSON.parse(lists);
+        } else {
+            return [];
+        }
     }
-    const [Inputdata,setInputdata]= useState("");
-    const [items,setitems]= useState(initial);
-    const [togglebtn,settogglebtn]=useState(false);
-    const [Editele,setEditele]= useState("")
-    const additems = ()=>{
-        if(!Inputdata){
+    const [Inputdata, setInputdata] = useState("");
+    const [items, setitems] = useState(initial);
+    const [togglebtn, settogglebtn] = useState(false);
+    const [Editele, setEditele] = useState("")
+    const additems = () => {
+        if (!Inputdata) {
             alert("Please Enter Some value")
-        }else if(Inputdata && togglebtn){
+        } else if (Inputdata && togglebtn) {
             settogglebtn(false);
             setInputdata(Inputdata);
-            const updated = items.map((ele)=>{
-                if(ele.id===Editele.id){
+            const updated = items.map((ele) => {
+                if (ele.id === Editele.id) {
                     ele.name = Inputdata;
                     setInputdata("")
                 }
@@ -28,7 +28,7 @@ const Todo = () => {
             })
             setitems(updated)
         }
-        else{
+        else {
             let up = {
                 id: new Date().getTime().toString(),
                 name: Inputdata
@@ -36,42 +36,48 @@ const Todo = () => {
 
             setitems([...items, up]);
             setInputdata("")
-        }   
+        }
     }
-    const deleted = (delid)=>{
-        const updated = items.filter((ele)=>{
-            return ele.id!=delid;
+    const deleted = (delid) => {
+        const status = window.confirm('Do you want to delete?')
+        if (!status) {
+            return 0;
+        }
+        const updated = items.filter((ele) => {
+            return ele.id !== delid;
         })
         setitems(updated)
     }
-    const edited = (editid)=>{
-        const edit_item = items.find((ele)=>{
+    const edited = (editid) => {
+        const edit_item = items.find((ele) => {
             return editid === ele.id;
         })
         setEditele(edit_item)
         settogglebtn(true);
         setInputdata(edit_item.name)
     }
-    useEffect(()=>{
-        localStorage.setItem("Todos",JSON.stringify(items))
-    },[items])
-  return (
-    <>
-    <div className='container'>
-        <div className='text-center mt-5'>
-            <h2 className='text-light fw-bold'>Todo List</h2>
-        </div>
-        <div className='mx-auto text-center py-4'>
-            <input type="text" className='border border-2 border-primary rounded-pill w-25 py-2 px-3' value={Inputdata} onChange={(event)=>{setInputdata(event.target.value)}}
-             placeholder='Enter the task'/>
-            <button className='btn btn-primary mx-3' onClick={()=>{additems()}}>
-                {togglebtn ===true ? <i className="fa fa-pencil-square-o" aria-hidden="true"></i>:<i className="fas fa-plus" aria-hidden="true"></i>}
-                </button>
-        </div>
-    </div>
-    <Todoitems items={items} deleted={deleted} edited = {edited}/>
-    </>
-  )
+    useEffect(() => {
+        localStorage.setItem("Todos", JSON.stringify(items))
+    }, [items])
+    return (
+        <>
+            <div className='container'>
+                <div className='text-center mt-5'>
+                    <h2 className='text-light fw-bold'>Todo List</h2>
+                </div>
+                <div className='row flex-lg-row  text-center py-4 justify-content-center align-items-center'>
+                    <div className='col-10 col-lg-5'>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" value={Inputdata} onChange={(event) => { setInputdata(event.target.value) }}
+                                placeholder='Enter the task' aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                            <span class="input-group-text btn btn-primary" onClick={() => { additems() }} id="basic-addon2">{togglebtn === true ? <i className="fa fa-pencil-square-o" aria-hidden="true"></i> : <i className="fas fa-plus" aria-hidden="true"></i>}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Todoitems items={items} deleted={deleted} edited={edited} />
+        </>
+    )
 }
 
 export default Todo
--- src/components/Todoitems.js
+++ src/components/Todoitems.js
@@ -1,34 +1,35 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Todoitems = ({items,deleted,edited}) => {
-  const [hover,sethover]=useState('bg-light')
-  const colorChange = (e)=>{
+const Todoitems = ({ items, deleted, edited }) => {
+  const colorChange = (e) => {
     e.target.classList.toggle('text-primary');
-    console.log("1")
     // e.target.className.toggle('bg-success')
   }
   return (
-  <>
-    {
-      items.map((value)=>{
-        return(
-                 <div className='container mt-4 mx-auto row justify-content-center align-item-center w-50 bg-light rounded-pill' key={value.id}>
-                <div className='col-6 fs-5 my-auto'><div className='align-middle'>{value.name}
-                  </div>
-                </div> 
-                <div className="col-4 hover">
-                    <span className='float-end btn fs-4' onClick={()=>{edited(value.id)}} >
-                      <i className="fa fa-pencil-square-o" aria-hidden="true" onMouseOver={(event)=>{colorChange(event)}} onMouseLeave={(event)=>{colorChange(event)}}></i>
-                      </span>
-                    <span className='float-end btn fs-4' onClick={()=>{deleted(value.id)}} >
-                      <i className="fas fa-trash" aria-hidden="true" onMouseOver={(event)=>{colorChange(event)}} onMouseLeave={(event)=>{colorChange(event)}}></i>
-                      </span>
-                </div>
-                </div>)
-            })
-          }
-          </>
-      
+    <>
+    <div className="container">
+      {
+        items.map((value) => {
+          return (
+            <div className='mt-4 mx-auto row justify-content-center text-center align-item-center bg-light rounded-3' key={value.id}>
+              <div className='col-7 fs-5 my-auto'>
+                <div className='align-middle'>{value.name}</div>
+              </div>
+              <div className="col-5 hover">
+                <span className='btn fs-4' onClick={() => { edited(value.id) }} >
+                  <i className="fa fa-pencil-square-o" aria-hidden="true" onMouseOver={(event) => { colorChange(event) }} onMouseLeave={(event) => { colorChange(event) }}></i>
+                </span>
+                <span className='btn fs-4' onClick={() => { deleted(value.id) }} >
+                  <i className="fas fa-trash" aria-hidden="true" onMouseOver={(event) => { colorChange(event) }} onMouseLeave={(event) => { colorChange(event) }}></i>
+                </span>
+              </div>
+            </div>)
+        })
+      }
+
+    </div>
+    </>
+
   )
 }
 
